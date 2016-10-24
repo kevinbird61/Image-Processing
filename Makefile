7c9f498d8d4760173e5762b110dfda1c028dabe4
kevinbird61 <kevinbird61@gmail.com>
2016-10-24 20:11:47 +0800

    Upload git-hook to maintain code-style.

--- Makefile
+++ Makefile
@@ -1,24 +1,29 @@
 TARGET:=bmpreader
+GIT_HOOKS := .git/hooks/pre-commit
 
-all:
+format:
+	astyle --style=kr --indent=spaces=4 --indent-switches --suffix=none *.[ch]
+
+all: format
 	gcc main.c -o $(TARGET)
 
-split:
-	gcc main.c -DSPLIT=7 -o $(TARGET)
-
 # Gaussian blur
-# 1: 5x5 with r,g,b 3 array
+# 1: 5x5 with r,g	,b 3 array
 # 2: 5x5 with original structure
-gau_blur_tri:
+gau_blur_tri: $(GIT_HOOKS) format
 	gcc main.c -DSPLIT=7 -DGAUSSIAN=1 -o $(TARGET)
 
-gau_blur_ori:
+gau_blur_ori: $(GIT_HOOKS) format
 	gcc main.c -DGAUSSIAN=2 -o $(TARGET)
 
 run:
 	# img/wf.bmp => has the 4 element(alpha value)
 	./$(TARGET) img/input.bmp output.bmp
 	eog output.bmp
+
+$(GIT_HOOKS):
+	@scripts/install-git-hooks
+	@echo
 
 clean:
 	rm *output.bmp $(TARGET)
--- image_op.c
+++ image_op.c
@@ -2,49 +2,51 @@
 #define IMAGE_OPERATION
 
 unsigned char gaussian55[25] = {
-  1,  4,  7,  4, 1,
-  4, 16, 26, 16, 4,
-  7, 26, 41, 26, 7,
-  4, 16, 26, 16, 4,
-  1,  4,  7,  4, 1,
+    1,  4,  7,  4, 1,
+    4, 16, 26, 16, 4,
+    7, 26, 41, 26, 7,
+    4, 16, 26, 16, 4,
+    1,  4,  7,  4, 1,
 };
 
-void naive_gaussian_blur_5(unsigned char *src,int w,int h){
-  for(int j=2;j<h-2;j++){
-    for(int i=2;i<w-2;i++){
-      int sum = 0;
-      int index = 0;
-      for(int sqr_j=j-2;sqr_j<j+3;sqr_j++){
-        for(int sqr_i=i-2;sqr_i<i+3;sqr_i++){
-          sum += src[sqr_j*w+sqr_i]*gaussian55[index++];
+void naive_gaussian_blur_5(unsigned char *src,int w,int h)
+{
+    for(int j=2; j<h-2; j++) {
+        for(int i=2; i<w-2; i++) {
+            int sum = 0;
+            int index = 0;
+            for(int sqr_j=j-2; sqr_j<j+3; sqr_j++) {
+                for(int sqr_i=i-2; sqr_i<i+3; sqr_i++) {
+                    sum += src[sqr_j*w+sqr_i]*gaussian55[index++];
+                }
+            }
+            sum /= 273;
+            if(sum > 255)
+                sum = 255;
+            src[j*w+i] = sum;
         }
-      }
-      sum /= 273;
-      if(sum > 255)
-        sum = 255;
-      src[j*w+i] = sum;
     }
-  }
 }
 
-void naive_gaussian_blur_5_original(RGBTRIPLE *src,int w,int h){
-  for(int j=2;j<h-2;j++){
-    for(int i=2;i<w-2;i++){
-      int sum_r = 0,sum_g = 0, sum_b = 0;
-      int index = 0;
-      for(int sqr_j=j-2;sqr_j<j+3;sqr_j++){
-        for(int sqr_i=i-2;sqr_i<i+3;sqr_i++){
-          sum_r += src[sqr_j*w+sqr_i].rgbRed*gaussian55[index];
-          sum_g += src[sqr_j*w+sqr_i].rgbGreen*gaussian55[index];
-          sum_b += src[sqr_j*w+sqr_i].rgbBlue*gaussian55[index];
-          index++;
+void naive_gaussian_blur_5_original(RGBTRIPLE *src,int w,int h)
+{
+    for(int j=2; j<h-2; j++) {
+        for(int i=2; i<w-2; i++) {
+            int sum_r = 0,sum_g = 0, sum_b = 0;
+            int index = 0;
+            for(int sqr_j=j-2; sqr_j<j+3; sqr_j++) {
+                for(int sqr_i=i-2; sqr_i<i+3; sqr_i++) {
+                    sum_r += src[sqr_j*w+sqr_i].rgbRed*gaussian55[index];
+                    sum_g += src[sqr_j*w+sqr_i].rgbGreen*gaussian55[index];
+                    sum_b += src[sqr_j*w+sqr_i].rgbBlue*gaussian55[index];
+                    index++;
+                }
+            }
+            src[j*w+i].rgbRed = ((sum_r/273) > 255 ) ? 255 : sum_r/273 ;
+            src[j*w+i].rgbGreen = ((sum_g/273) > 255 ) ? 255 : sum_g/273 ;
+            src[j*w+i].rgbBlue = ((sum_b/273) > 255 ) ? 255 : sum_b/273 ;
         }
-      }
-      src[j*w+i].rgbRed = ((sum_r/273) > 255 ) ? 255 : sum_r/273 ;
-      src[j*w+i].rgbGreen = ((sum_g/273) > 255 ) ? 255 : sum_g/273 ;
-      src[j*w+i].rgbBlue = ((sum_b/273) > 255 ) ? 255 : sum_b/273 ;
     }
-  }
 }
 
 #endif
