cc0555f965c885226c38b2ba6e77f411590fca54
CheHsuan <exas60807@gmail.com>
2016-10-29 16:51:43 +0800

    add hsv operations
    enable to brighten/darken or saturate/disaturate the image

---
+++ hsv.h
@@ -0,0 +1,14 @@
+#ifndef HSV
+#define HSV 
+#include <stdlib.h>
+#include  "bmp.h"
+
+typedef struct _HSV {
+    float h;
+    float s;
+    float v;
+} HSVTRIPLE;
+
+void change_brightness(RGBTRIPLE *src, float brightness, int w, int h);
+void change_saturation(RGBTRIPLE *src, float saturation, int w, int h);
+#endif
